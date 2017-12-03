76773a56b4fab2e5af4e59ebfb4220f54194cb8b
akira345 <g.akira345@gmail.com>
2017-12-03 15:24:57 +0900

    アセンブリ情報修正

--- UserControlSample/Properties/AssemblyInfo.cs
+++ UserControlSample/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("UserControlSample")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ユーザコントロールフォームを使うサンプル")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UserControlSample")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Akira345 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
